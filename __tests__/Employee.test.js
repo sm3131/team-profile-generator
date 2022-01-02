0a62f863026dc5b93e0c752d4c3881c2a145b5fe
sm3131 <sm2683@nau.edu>
2022-01-02 14:50:35 -0700

    added required packages for test and index files and created first failing test for Employee

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Joe', 'email@email.com');
+
+    expect(employee.name).toBe('Joe');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('email@email.com');
+});
