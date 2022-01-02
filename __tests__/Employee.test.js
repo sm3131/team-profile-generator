294e71a68f34a12a587bb6938ec8e68ee0bf8133
sm3131 <sm2683@nau.edu>
2022-01-02 15:22:23 -0700

    created test for getName function on employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,9 @@ test('creates an employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe('email@email.com');
 });
+
+test('gets employee name', () => {
+    const employee = new Employee('Joe', 'email@email.com');
+
+    expect(employee.getName()).toBe('Joe');
+});
