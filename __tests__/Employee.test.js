aee4c9eaf3ba9db112c7184458431a0a130f08a8
sm3131 <sm2683@nau.edu>
2022-01-02 15:35:59 -0700

    created tests for getEmail and getRole, then wrote minimum required code to pass these tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,21 +1,34 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Joe', 'email@email.com');
+    const employee = new Employee('Joe', 'email@email.com', 'Employee');
 
     expect(employee.name).toBe('Joe');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe('email@email.com');
+    expect(employee.role).toBe('Employee');
 });
 
 test('gets employee name', () => {
-    const employee = new Employee('Joe', 'email@email.com');
+    const employee = new Employee('Joe', 'email@email.com', 'Employee');
 
     expect(employee.getName()).toBe('Joe');
 });
 
 test('gets employee id', () => {
-    const employee = new Employee('Joe', 'email@email.com');
+    const employee = new Employee('Joe', 'email@email.com', 'Employee');
 
     expect(employee.getId()).toEqual(expect.any(Number));
-})
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Joe', 'email@email.com', 'Employee');
+
+    expect(employee.getEmail()).toBe('email@email.com');
+});
+
+test('gets employee role', () => {
+    const employee = new Employee('Joe', 'email@email.com', 'Employee');
+
+    expect(employee.getRole()).toBe('Employee');
+});
