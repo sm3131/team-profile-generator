99c908efc5b615faa2f42df77ef6389e97951f86
sm3131 <sm2683@nau.edu>
2022-01-06 02:56:27 -0700

    created tests for intern and created class and functions to pass tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,22 @@
 const Intern = require('../lib/Intern');
+
+test('creates an intern object', () => {
+    const intern = new Intern('Rex', 2, 'email@email.com', 'school', 'Intern');
+
+    expect(intern.name).toBe('Rex');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toBe('email@email.com');
+    expect(intern.school).toBe('school');
+    expect(intern.role).toBe('Intern');
+})
+
+test("gets school attended by intern", () => {
+    const intern = new Intern('Rex', 2, 'email@email.com', 'school', 'Intern');
+    expect(intern.getSchool()).toBe('school');
+})
+
+test("gets intern's role", () => {
+    const intern = new Intern('Rex', 2, 'email@email.com', 'school', 'Intern');
+
+    expect(intern.getRole()).toBe('Intern');
+})
