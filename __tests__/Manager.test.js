3abe0b38ab4e466ffea27af969d28823c9c43ddb
sm3131 <sm2683@nau.edu>
2022-01-07 02:20:24 -0700

    refactored Manager class to extend Employee class constructor

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,13 +1,19 @@
 const Manager = require('../lib/Manager');
 
 test('creates a manger object', () => {
-    const manager = new Manager('Jill', 1, 'email@email.com', 10, 'Manager');
+    const manager = new Manager('Jill', 2, 'email@email.com', 10, 'Manager');
 
     expect(manager.name).toBe('Jill');
-    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.id).toEqual(2);
     expect(manager.email).toBe('email@email.com');
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toEqual(10);
     expect(manager.role).toBe('Manager');
+})
+
+test("gets manager's officeNumber", () => {
+    const manager = new Manager('Jill', 1, 'email@email.com', 10, 'Manager');
+
+    expect(manager.getOfficeNumber()).toEqual(10);
 })
 
 test("gets manager's role", () => {
