73462f661939c5acd37248a4e92dca4adfe607e6
sm3131 <sm2683@nau.edu>
2022-01-07 02:25:25 -0700

    refactored Engineer class to extend Employee constructor class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,22 +1,22 @@
 const Engineer = require('../lib/Engineer');
 
 test('creates an engineer object', () => {
-    const engineer = new Engineer('Sanni', 2, 'email@email.com', 'github', 'Engineer');
+    const engineer = new Engineer('Sanni', 3, 'email@email.com', 'github', 'Engineer');
 
     expect(engineer.name).toBe('Sanni');
-    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.id).toEqual(3);
     expect(engineer.email).toBe('email@email.com');
     expect(engineer.github).toBe('github');
     expect(engineer.role).toBe('Engineer');
 })
 
 test("gets engineer's github", () => {
-    const engineer = new Engineer('Sanni', 2, 'email@email.com', 'github', 'Engineer');
+    const engineer = new Engineer('Sanni', 3, 'email@email.com', 'github', 'Engineer');
     expect(engineer.getGithub()).toBe('github');
 })
 
 test("gets engineer's role", () => {
-    const engineer = new Engineer('Sanni', 2, 'email@email.com', 'github', 'Engineer');
+    const engineer = new Engineer('Sanni', 3, 'email@email.com', 'github', 'Engineer');
 
     expect(engineer.getRole()).toBe('Engineer');
 })
