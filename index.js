2708217bb070e4b9e229023e6962f763eff6b481
sm3131 <sm2683@nau.edu>
2022-01-10 03:24:29 -0700

    added validation to every user prompt question to ensure only accept valid answers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const teamArray = [];
 function StartPrompts() {
     console.log("Welcome to the team profile generator! With this application you can enter your team member's names and information in order to create a team profile webpage.");
 
-        inquirer
+    inquirer
         .prompt({
             type: 'confirm',
             name: 'confirmPrompts',
@@ -18,7 +18,7 @@ function StartPrompts() {
         })
         .then(answer => {
             if (answer.confirmPrompts === true) {
-               this.workerPrompts();
+                this.workerPrompts();
             } else {
                 return false;
             }
@@ -28,62 +28,93 @@ function StartPrompts() {
 StartPrompts.prototype.workerPrompts = function () {
     return inquirer
         .prompt([
-        {
-            type:'list',
-            name:'role',
-            message:"Please confirm the worker's role by hitting enter",
-            choices:['Manager']
-        },
-        {
-            type: 'text',
-            name: 'managerName',
-            message: "What is the team manager's name?"
-        },
-        {
-            type: 'text',
-            name: 'managerId',
-            message: "What is the manager's employee ID number?"
-        },
-        {
-            type: 'text',
-            name: 'managerEmail',
-            message: "What is the manager's email address?"
-        },
-        {
-            type: 'text',
-            name: 'officeNumber',
-            message: "What is the manager's office number?"
-        }
-    ])
-    .then(managerInfo => {
-        let name = managerInfo.managerName;
-        let id = managerInfo.managerId;
-        let email = managerInfo.managerEmail;
-        let officeNumber = managerInfo.officeNumber;
-        let role = managerInfo.role;
+            {
+                type: 'list',
+                name: 'role',
+                message: "Please confirm the worker's role by hitting enter",
+                choices: ['Manager']
+            },
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the team manager's name?",
+                validate: managerName => {
+                    if (managerName) {
+                        return true;
+                    } else {
+                        console.log("Please enter the team manager's name!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: "What is the manager's employee ID number?",
+                validate: value => {
+                    let pass = !isNaN(value)
+                    if (pass && value) {
+                        return true
+                    }
+                    return "Please enter a valid number for the manager's employee ID number!"
+                }
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is the manager's email address?",
+                validate: value => {
+                    let include = value.includes("@");
+                    if (include && value) {
+                        return true;
+                    } else {
+                        console.log("\nPlease enter a valid email for the team manager!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is the manager's office number?",
+                validate: value => {
+                    let pass = !isNaN(value)
+                    if (pass && value) {
+                        return true
+                    }
+                    return "Please enter a valid number for the manager's office number!"
+                }
+            }
+        ])
+        .then(managerInfo => {
+            let name = managerInfo.managerName;
+            let id = managerInfo.managerId;
+            let email = managerInfo.managerEmail;
+            let officeNumber = managerInfo.officeNumber;
+            let role = managerInfo.role;
 
-        console.log(name, id, email, officeNumber, role);
+            console.log(name, id, email, officeNumber, role);
 
-        let manager = new Manager(name, id, email, officeNumber, role);
-        teamArray.push(manager);
-        console.log(teamArray);
+            let manager = new Manager(name, id, email, officeNumber, role);
+            teamArray.push(manager);
+            console.log(teamArray);
 
-        this.addTeamMember();
-    })
+            this.addTeamMember();
+        })
 }
 
-StartPrompts.prototype.addTeamMember = function() {
-    return inquirer 
-            .prompt({
-                type:'list',
-                name:'addWorkers',
-                message:'If you would like to add a team member select from the list below. If not select finish building team to create your webpage.',
-                choices:['Engineer', 'Intern', 'Finish Building Team'],
-                default:'Finish Building Team'
-            })
-        .then(({addWorkers}) => {
+StartPrompts.prototype.addTeamMember = function () {
+    return inquirer
+        .prompt({
+            type: 'list',
+            name: 'addWorkers',
+            message: 'If you would like to add a team member select from the list below. If not select finish building team to create your webpage.',
+            choices: ['Engineer', 'Intern', 'Finish Building Team'],
+            default: 'Finish Building Team'
+        })
+        .then(({ addWorkers }) => {
             console.log(addWorkers);
-            if(addWorkers === 'Engineer'){
+            if (addWorkers === 'Engineer') {
                 this.addEngineer();
             } else if (addWorkers === 'Intern') {
                 this.addIntern();
@@ -94,38 +125,70 @@ StartPrompts.prototype.addTeamMember = function() {
         })
 }
 
-StartPrompts.prototype.addEngineer = function() {
+StartPrompts.prototype.addEngineer = function () {
 
     return inquirer
         .prompt([
             {
-                type:'list',
-                name:'role',
-                message:"Please confirm the worker's role by hitting enter",
-                choices:['Engineer']
+                type: 'list',
+                name: 'role',
+                message: "Please confirm the worker's role by hitting enter",
+                choices: ['Engineer']
             },
             {
-                type:'text',
-                name:'engineerName',
-                message:"What is the engineer's name?"
+                type: 'input',
+                name: 'engineerName',
+                message: "What is the engineer's name?",
+                validate: value => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter the engineer's name!");
+                        return false;
+                    }
+                }
             },
             {
-                type: 'text',
+                type: 'input',
                 name: 'engineerId',
-                message: "What is the engineer's employee ID number?"
+                message: "What is the engineer's employee ID number?",
+                validate: value => {
+                    let pass = !isNaN(value)
+                    if (pass && value) {
+                        return true
+                    }
+                    return "Please enter a valid number for the engineer's employee ID number!"
+                }
             },
             {
-                type: 'text',
+                type: 'input',
                 name: 'engineerEmail',
-                message: "What is the engineer's email address?"
+                message: "What is the engineer's email address?",
+                validate: value => {
+                    let include = value.includes("@");
+                    if (include && value) {
+                        return true;
+                    } else {
+                        console.log("\nPlease enter a valid email for the engineer!");
+                        return false;
+                    }
+                }
             },
             {
-                type: 'text',
+                type: 'input',
                 name: 'github',
-                message: "What is the engineer's GitHub username?"
+                message: "What is the engineer's GitHub username?",
+                validate: value => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter the engineer's github username!");
+                        return false;
+                    }
+                }
             }
         ])
-        
+
         .then(engineerInfo => {
             let name = engineerInfo.engineerName;
             let id = engineerInfo.engineerId;
@@ -138,40 +201,72 @@ StartPrompts.prototype.addEngineer = function() {
             let engineer = new Engineer(name, id, email, github, role);
             teamArray.push(engineer);
             console.log(teamArray);
-    
+
             this.addTeamMember();
         })
 }
 
-StartPrompts.prototype.addIntern = function() {
+StartPrompts.prototype.addIntern = function () {
 
     return inquirer
         .prompt([
             {
-                type:'list',
-                name:'role',
-                message:"Please confirm the worker's role by hitting enter",
-                choices:['Intern']
+                type: 'list',
+                name: 'role',
+                message: "Please confirm the worker's role by hitting enter",
+                choices: ['Intern']
             },
             {
-                type:'text',
-                name:'internName',
-                message:"What is the intern's name?"
+                type: 'input',
+                name: 'internName',
+                message: "What is the intern's name?",
+                validate: value => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's name!");
+                        return false;
+                    }
+                }
             },
             {
-                type: 'text',
+                type: 'input',
                 name: 'internId',
-                message: "What is the intern's employee ID number?"
+                message: "What is the intern's employee ID number?",
+                validate: value => {
+                    let pass = !isNaN(value)
+                    if (pass && value) {
+                        return true
+                    }
+                    return "Please enter a valid number for the intern's employee ID number!"
+                }
             },
             {
-                type: 'text',
+                type: 'input',
                 name: 'internEmail',
-                message: "What is the intern's email address?"
+                message: "What is the intern's email address?",
+                validate: value => {
+                    let include = value.includes("@");
+                    if (include && value) {
+                        return true;
+                    } else {
+                        console.log("\nPlease enter a valid email for the intern!");
+                        return false;
+                    }
+                }
             },
             {
-                type: 'text',
+                type: 'input',
                 name: 'school',
-                message: "What school did the intern attend?"
+                message: "What school did the intern attend?",
+                validate: value => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter the school the intern attended!");
+                        return false;
+                    }
+                }
             },
         ])
 
@@ -181,21 +276,22 @@ StartPrompts.prototype.addIntern = function() {
             let email = internInfo.internEmail;
             let school = internInfo.school;
             let role = internInfo.role;
-    
+
             console.log(name, id, email, school, role);
 
             let intern = new Intern(name, id, email, school, role);
             teamArray.push(intern);
             console.log(teamArray);
-    
+
             this.addTeamMember();
         })
 }
-    const workerArray = [];
+const workerArray = [];
 
-    StartPrompts.prototype.finishTeam = function(team) {
+StartPrompts.prototype.finishTeam = function (team) {
 
     console.log(team)
+
     team.forEach(getInfo);
 
     function getInfo(value) {
@@ -204,12 +300,12 @@ StartPrompts.prototype.addIntern = function() {
             let manager = createManager(value.name, value.role, value.id, value.email, value.officeNumber)
             console.log(manager);
             workerArray.push(manager);
-            
+
         } else if (value.role === "Engineer") {
             let engineer = createEngineer(value.name, value.role, value.id, value.email, value.github)
             console.log(engineer);
             workerArray.push(engineer);
-        
+
         } else if (value.role === "Intern") {
             let intern = createIntern(value.name, value.role, value.id, value.email, value.school)
             console.log(intern);
@@ -218,25 +314,25 @@ StartPrompts.prototype.addIntern = function() {
         let finalHTML = generateHTML(workerArray);
         console.log(finalHTML);
         writeFile(finalHTML)
-        .catch(err => {
-            console.log(err);
-        })
+            .catch(err => {
+                console.log(err);
+            })
     }
 }
 
 const writeFile = finalHTML => {
     return new Promise((resolve, reject) => {
         fs.writeFile('./dist/new_index.html', finalHTML, err => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve({
-          ok: true,
-          message: 'Team Profile WebPage Created!'
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Team Profile WebPage Created!'
+            });
         });
-      });
     });
-  };
+};
 
 new StartPrompts()
