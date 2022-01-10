972af41831096b7b3f8f71cbf1771816e580e1bc
sm3131 <sm2683@nau.edu>
2022-01-09 21:46:11 -0700

    created functions to send user input to generate html functions and then write final html file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const { createManager, createEngineer, createIntern, generateHTML } = require('./src/generateHTML')
+const { createManager, createEngineer, createIntern, generateHTML } = require('./src/generateHTML');
 const teamArray = [];
 
 function StartPrompts() {
